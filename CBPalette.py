28b7f9c2f5c5b614aa62eb8b5c943b4f2f60c335
Steve Wilson <60525163+stw4198@users.noreply.github.com>
2020-12-10 15:42:50 +0000

    Addition colour palettes and formatting

--- CBPalette.py
+++ CBPalette.py
@@ -1,19 +1,76 @@
 #Colour palettes by Paul Tol https://personal.sron.nl/~pault/#sec:qualitative, https://cran.r-project.org/web/packages/khroma/vignettes/tol.html
+CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+                  '#f781bf', '#a65628', '#984ea3',
+                  '#999999', '#e41a1c', '#dede00']
 
-Tol_bright_colour = ['#4477AA','#66CCEE','#228833','#CCBB44','#EE6677','#AA3377','#BBBBBB']
+Tol_bright_colour = ['#4477AA','#66CCEE','#228833',
+                     '#CCBB44','#EE6677','#AA3377','#BBBBBB']
 
 Tol_high_C = ['#FFFFFF','#DDAA33','#BB5566','#004488','#000000']
 
-Tol_muted = ['#332288','#88CCEE','#44AA99','#117733','#999933','#DDCC77','#CC6677','#882255','#AA4499','#DDDDDD']
+Tol_muted = ['#332288','#88CCEE','#44AA99',
+             '#117733','#999933','#DDCC77',
+             '#CC6677','#882255','#AA4499','#DDDDDD']
 
-Tol_vibrant = ['#0077BB','#33BBEE','#009988','#EE7733','#CC3311','#EE3377','#BBBBBB']
+Tol_vibrant = ['#0077BB','#33BBEE','#009988',
+               '#EE7733','#CC3311','#EE3377','#BBBBBB']
 
-Tol_pale = ['#BBCCEE','#CCEEFF','#CCDDAA','#EEEEBB','#FFCCCC','#DDDDDD']
+Tol_pale = ['#BBCCEE','#CCEEFF','#CCDDAA',
+            '#EEEEBB','#FFCCCC','#DDDDDD']
 
-Tol_dark = ['#222255','#225555','#225522','#666633','#663333','#555555']
+Tol_dark = ['#222255','#225555','#225522',
+            '#666633','#663333','#555555']
 
-Tol_light = ['#77AADD','#99DDFF','#44BB99','#BBCC33','#AAAA00','#EEDD88','#EE8866','#FFAABB','#DDDDDD']
+Tol_light = ['#77AADD','#99DDFF','#44BB99',
+             '#BBCC33','#AAAA00','#EEDD88',
+             '#EE8866','#FFAABB','#DDDDDD']
+
+Tol_sunet_discrete = ['#364B9A','#4A7BB7','#6EA6CD',
+                      '#98CAE1','#C2E4EF','#EAECCC',
+                      '#FEDA8B','#FDB366','#F67E4B',
+                      '#DD3D2D','#A50026']
+
+Tol_sunset = ['#364B9A', '#4A7BB7', '#6EA6CD', 
+              '#98CAE1', '#C2E4EF', '#EAECCC', 
+              '#FEDA8B', '#FDB366', '#F67E4B', 
+              '#DD3D2D', '#A50026']
+
+Tol_BrRd_discrete = ['#2166AC', '#4393C3', '#92C5DE',
+                     '#D1E5F0', '#F7F7F7','#FDDBC7',
+                     '#F4A582', '#D6604D', '#B2182B']
+
+Tol_BrRd =  ['#2166AC', '#4393C3', '#92C5DE',
+             '#D1E5F0', '#F7F7F7', '#FDDBC7',
+             '#F4A582', '#D6604D', '#B2182B']
+
+Tol_PRGn_discrete = ['#762A83', '#9970AB', '#C2A5CF',
+                     '#E7D4E8', '#F7F7F7', '#D9F0D3',
+                     '#ACD39E', '#5AAE61', '#1B7837']
+
+Tol_PRGn = ['#762A83', '#9970AB', '#C2A5CF',
+            '#E7D4E8', '#F7F7F7', '#D9F0D3',
+            '#ACD39E', '#5AAE61', '#1B7837']
+
+Tol_YlOrBr_discrete = ['#FFFFE5', '#FFF7BC', '#FEE391',
+                       '#FEC44F', '#FB9A29', '#EC7014',
+                       '#CC4C02', '#993404', '#662506']
+
+Tol_YlOrBr = ['#FFFFE5', '#FFF7BC', '#FEE391',
+              '#FEC44F', '#FB9A29', '#EC7014',
+              '#CC4C02', '#993404', '#662506']
+
+Tol_WhOrBr = ['#FFFFFF', '#FFF7BC', '#FEE391',
+              '#FEC44F', '#FB9A29', '#EC7014',
+              '#CC4C02', '#993404', '#662506']
+
+Tol_iridescent = ['#FEFBE9', '#FCF7D5', '#F5F3C1', '#EAF0B5', '#DDECBF',
+                '#D0E7CA', '#C2E3D2', '#B5DDD8', '#A8D8DC', '#9BD2E1',
+                '#8DCBE4', '#81C4E7', '#7BBCE7', '#7EB2E4', '#88A5DD',
+                '#9398D2', '#9B8AC4', '#9D7DB2', '#9A709E', '#906388',
+                '#805770', '#684957', '#46353A']
 
 #Colour palette by Thøger Rivera-Thorsen https://gist.github.com/thriveth/8560036
 
-CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a','#f781bf', '#a65628', '#984ea3','#999999', '#e41a1c', '#dede00']
+CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a', 
+                  '#f781bf', '#a65628', '#984ea3', 
+                  '#999999', '#e41a1c', '#dede00']
