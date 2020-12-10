49ec5c22c2bac08cd2d468159aaa36f42894a826
Steve Wilson <60525163+stw4198@users.noreply.github.com>
2020-12-10 15:16:44 +0000

    Added palettes

--- CBPalette.py
+++ CBPalette.py
@@ -1,1 +1,19 @@
+#Colour palettes by Paul Tol https://personal.sron.nl/~pault/#sec:qualitative
 
+Tol_bright_colour = ['#4477AA','#66CCEE','#228833','#CCBB44','#EE6677','#AA3377','#BBBBBB']
+
+Tol_high_C = ['#FFFFFF','#DDAA33','#BB5566','#004488','#000000']
+
+Tol_muted = ['#332288','#88CCEE','#44AA99','#117733','#999933','#DDCC77','#CC6677','#882255','#AA4499','#DDDDDD']
+
+Tol_vibrant = ['#0077BB','#33BBEE','#009988','#EE7733','#CC3311','#EE3377','#BBBBBB']
+
+Tol_pale = ['#BBCCEE','#CCEEFF','#CCDDAA','#EEEEBB','#FFCCCC','#DDDDDD']
+
+Tol_dark = ['#222255','#225555','#225522','#666633','#663333','#555555']
+
+Tol_light = ['#77AADD','#99DDFF','#44BB99','#BBCC33','#AAAA00','#EEDD88','#EE8866','#FFAABB','#DDDDDD']
+
+#Colour palette by Thøger Rivera-Thorsen https://gist.github.com/thriveth/8560036
+
+CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a','#f781bf', '#a65628', '#984ea3','#999999', '#e41a1c', '#dede00']
